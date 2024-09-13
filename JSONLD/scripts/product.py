14968df98c9a2b80cb983a6ecaf2b01e62950851
Daniel Ellis <daniel.ellis@ext.esa.int>
2024-09-13 22:31:47 +0100

    generate activity-id

---
+++ JSONLD/scripts/product.py
@@ -0,0 +1,20 @@
+from __setup__ import * 
+
+lddata = cmipld.sync(lddata)
+
+name = 'product'
+
+frame = {
+        "@type": f"cmip6plusmip:core-descriptors",
+        "@embed":"@always",
+}
+
+
+frame = cmipld.Frame(lddata,frame).clean(['rmld','untag'])
+
+# data = cmipld.utils.sorted_dict(frame.key_value('name','description'))
+
+data = frame.data
+print(data)
+
+# cmipld.utils.wjsn(finalise(data,name),base+f'/CMIP6Plus_{name}.json')
