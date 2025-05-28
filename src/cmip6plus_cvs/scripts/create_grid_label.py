4effd2ebe58d4aa1d1ebb4a11182b6cd8998822f
TROUSSELLIER Laurent <laurent.troussellier@ipsl.fr>
2025-05-28 15:33:16 +0200

    fix : type grid_label into grid

--- src/cmip6plus_cvs/scripts/create_grid_label.py
+++ src/cmip6plus_cvs/scripts/create_grid_label.py
@@ -1,38 +1,35 @@
-import os
 import json
+import os
+
 # Directory where the JSON files will be saved
-save_dir = 'grid_label'
+save_dir = "grid_label"
 
 # Create the directory if it doesn't exist
 os.makedirs(save_dir, exist_ok=True)
 
-#the context : 
-ctx = {"@context": 
-        {
+# the context :
+ctx = {
+    "@context": {
         "id": "@id",
         "type": "@type",
-
-        "@base" : "https://espri-mod.github.io/mip-cmor-tables/grid_label/",
-        "grid_label":"https://espri-mod.github.io/mip-cmor-tables/grid_label"
-       }
-
-       }
+        "@base": "https://espri-mod.github.io/mip-cmor-tables/grid/",
+        "grid_label": "https://espri-mod.github.io/mip-cmor-tables/grid",
+    }
+}
 file_path = os.path.join(save_dir, "000_context.jsonld")
 
-with open(file_path, 'w') as f:
+with open(file_path, "w") as f:
     json.dump(ctx, f, indent=4)
 
 
 # TERMS : TODO ... check if it appears in CMIP6Plus
 
-terms = ["gn","gr"]
+terms = ["gn", "gr"]
 
 for term in terms:
-    data = {"@context":"000_context.jonld",
-            "id" : term,
-            "type": "grid_label"}
+    data = {"@context": "000_context.jonld", "id": term, "type": "grid"}
 
     file_path = os.path.join(save_dir, f"{term}.json")
 
-    with open(file_path, 'w') as f:
+    with open(file_path, "w") as f:
         json.dump(data, f, indent=4)
