98119c667ef702b581217f5e51d885759f33cbfd
Matthew Mizielinski <matthew.mizielinski@metoffice.gov.uk>
2026-01-27 10:06:24 +0000

    #152: add CERESMIP experiments

--- src/add_expt.py
+++ src/add_expt.py
@@ -4,20 +4,16 @@ Script used for bulk adding experiments
 """
 # experiment id	activity id	experiment	tier	sub experiment id	parent experiment id	required model components	additional allowed model components	start year	end year	min number of years	parent activity id	description
 data = \
-"""esm-up2p0-gwl6p0	TIPMIP	zero CO2 emissions simulation starting at global warming level of 6 K, branching off from esm-up2p0	2	none	esm-up2p0	AOGCM BGC	AER CHEM	 	 	300	TIPMIP	stabilisation at GWL6
-esm-up2p0-gwl3p0-50y-dn2p0	TIPMIP	ramp down from global warming level of 3K after 50 years, with CO2 emissions that are the negative of those used in esm-up2p0	2	none	esm-up2p0-gwl3p0	AOGCM BGC	AER CHEM	 	 	150	TIPMIP	ramp-down after GWL3
-esm-up2p0-gwl1p5-50y-dn2p0	TIPMIP	ramp down from global warming level of 1.5K after 50 years, with CO2 emissions that are the negative of those used in esm-up2p0	2	none	esm-up2p0-gwl1p5	AOGCM BGC	AER CHEM	 	 	75	TIPMIP	ramp-down after GWL1.5
-esm-up2p0-gwl3p0-50y-dn2p0-gwl2p0	TIPMIP	zero CO2 emissions simulation branching off from esm-up2p0-gwl3p0-50y-dn2p0 after it passes the 2 K global warming level	2	none	esm-up2p0-gwl3p0-50y-dn2p0	AOGCM BGC	AER CHEM	 	 	300	TIPMIP	stabilisation at GWL2 after temporary overshoot to GWL3
-esm-up2p0-gwl3p0-50y-dn2p0-gwl1p5	TIPMIP	zero CO2 emissions simulation branching off from esm-up2p0-gwl3p0-50y-dn2p0 after it passes the 1.5 K global warming level	2	none	esm-up2p0-gwl3p0-50y-dn2p0	AOGCM BGC	AER CHEM	 	 	300	TIPMIP	  stabilisation at GWL1.5 after temporary overshoot to GWL3
-esm-up2p0-gwl4p0-50y-dn2p0-gwl0p0	TIPMIP	zero CO2 emissions simulation branching off from esm-up2p0-gwl4p0-50y-dn2p0 after it passes the 0 K global warming level (piControl)	2	none	esm-up2p0-gwl4p0-50y-dn2p0	AOGCM BGC	AER CHEM	 	 	300	TIPMIP	stabilisation at piControl after temporary overshoot to GWL4
-esm-up2p0-gwl3p0-50y-dn2p0-gwl0p0	TIPMIP	zero CO2 emissions simulation branching off from esm-up2p0-gwl3p0-50y-dn2p0 after it passes the 0 K global warming level (piControl)	2	none	esm-up2p0-gwl3p0-50y-dn2p0	AOGCM BGC	AER CHEM	 	 	300	TIPMIP	  stabilisation at piControl after temporary overshoot to GWL3
-esm-up2p0-gwl2p0-50y-dn2p0-gwl0p0	TIPMIP	zero CO2 emissions simulation branching off from esm-up2p0-gwl2p0-50y-dn2p0 after it passes the 0 K global warming level (piControl)	2	none	esm-up2p0-gwl2p0-50y-dn2p0	AOGCM BGC	AER CHEM	 	 	300	TIPMIP	stabilisation at piControl after temporary overshoot to GWL2
-esm-up2p0-gwl4p0-200y-dn2p0	TIPMIP	ramp down from global warming level of 4K after 200 years, with CO2 emissions that are the negative of those used in esm-up2p0	2	none	esm-up2p0-gwl4p0	AOGCM BGC	AER CHEM	 	 	200	TIPMIP	ramp-down after extended period of GWL4
-esm-up2p0-gwl3p0-200y-dn2p0	TIPMIP	ramp down from global warming level of 3K after 200 years, with CO2 emissions that are the negative of those used in esm-up2p0	2	none	esm-up2p0-gwl3p0	AOGCM BGC	AER CHEM	 	 	150	TIPMIP	ramp-down after extended period of GWL3
-esm-up2p0-gwl2p0-200y-dn2p0	TIPMIP	ramp down from global warming level of 2K after 200 years, with CO2 emissions that are the negative of those used in esm-up2p0	2	none	esm-up2p0-gwl2p0	AOGCM BGC	AER CHEM	 	 	100	TIPMIP	ramp-down after extended period of GWL2
-esm-up2p0-gwl4p0-50y-dn1p0	TIPMIP	ramp down from global warming level of 4K after 50 years, with CO2 emissions that are the negative and half the magnitude of those used in esm-up2p0	2	none	esm-up2p0-gwl4p0	AOGCM BGC	AER CHEM	 	 	400	TIPMIP	slow ramp-down after GWL4
-esm-up2p0-gwl3p0-50y-dn1p0	TIPMIP	ramp down from global warming level of 3K after 50 years, with CO2 emissions that are the negative and half magnitude of those used in esm-up2p0	2	none	esm-up2p0-gwl3p0	AOGCM BGC	AER CHEM	 	 	300	TIPMIP	slow ramp-down after GWL3
-esm-up2p0-gwl2p0-50y-dn1p0	TIPMIP	ramp down from global warming level of 2K after 50 years, with CO2 emissions that are the negative and half the magnitude of those used in esm-up2p0	2	none	esm-up2p0-gwl2p0	AOGCM BGC	AER CHEM	 	 	200	TIPMIP	slow ramp-down after GWL2"""
+"""amip-nudge	CERESMIP	Nudged amip	2	none	no parent	AGCM	AER CHEM	1990	2024	35	no parent	Nudged amip: All forcings (single run, nudged winds) with observed SST/SIC
+amip-nat	CERESMIP	Amip with Natural forcings	2	none	no parent	AGCM	AER CHEM	1990	2024	35	no parent	Amip with Natural forcings: Solar + volcanic + orbital with observed SST/SIC. All other boundary conditions set to 1850.
+amip-ghg	CERESMIP	AMIP with WMGHG-only	2	none	no parent	AGCM	AER CHEM	1990	2024	35	no parent	AMIP with WMGHG-only: Well-mixed greenhouse-gas-only simulations with observed SST/SIC
+amip-aer	CERESMIP	AMIP with Aer-only	2	none	no parent	AGCM	AER CHEM	1990	2024	35	no parent	AMIP with Aer-only: Anthropogenic-aerosol-only simulations with observed SST/SIC
+amip-slcf	CERESMIP	AMIP with SLCF-only	2	none	no parent	AGCM	AER CHEM	1990	2024	35	no parent	AMIP with SLCF-only: Anthropogenic changes in short-lived climate forcings (for use with models including interactive composition) with observed SST/SIC
+amip-sst	CERESMIP	AMIP with SST/SIC-only	2	none	no parent	AGCM	AER CHEM	1990	2024	35	no parent	AMIP with SST/SIC-only: Observed SST/SIC change only
+amip-noghg	CERESMIP	AMIP but no ghgs	2	none	no parent	AGCM	AER CHEM	1990	2024	35	no parent	AMIP but no ghgs: As with amip, but no changes in well-mixed GHGs
+amip-noaer	CERESMIP	AMIP but no aerosol changes	2	none	no parent	AGCM	AER CHEM	1990	2024	35	no parent	AMIP but no aerosol changes: As with amip, but no changes in aerosols"""
+
+
 output = {}
 for row in data.split("\n"):
     expt_id, activity_id, experiment, tier, sub_expt, parent_expt, req_model_comp, add_model_comp, start, end, min_years,parent_activity, description= row.split("\t")
@@ -46,4 +42,4 @@ with open('../CMIP6Plus_experiment_id.json') as fh:
 
 data["experiment_id"].update(output)
 with open('../CMIP6Plus_experiment_id.json', 'w') as fh:
-    json.dump(data, fh,  indent=4)
+    json.dump(data, fh,  indent=4, sort_keys=True)
